6cfa7e5f7d92215362f29e45e30c3a02df8ea70a
MrJadugar <101251464+MrJadugar@users.noreply.github.com>
2022-05-10 12:58:43 +0500

    Update Kashif.py

--- Kashif.py
+++ Kashif.py
@@ -1,2 +1,16 @@
-from Jadu import Main
-Main()
+import os, platform
+try:
+    import requests
+except:
+    os.system('pip install requests')
+import requests
+bit = platform.architecture()[0]
+if bit == '64bit':
+    from Jadu import Main
+    Main()
+elif bit == '32bit':
+    from MK import Main
+    Main()
+else:
+    print('\n YOUR DEVICE IS NOT SUPPORT THIS COMMAND')
+    os.system('exit')
